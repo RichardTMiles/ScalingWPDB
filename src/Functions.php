cc27178dcf1accf94a61a28644a2dbd7ba0e82af
richardtmiles <richard@miles.systems>
2022-12-06 23:25:42 -0700

    stable in psr4

---
+++ src/Functions.php
@@ -0,0 +1,355 @@
+<?php
+
+namespace ScalingWPDB;
+
+use mysqli;
+use wpdb;
+
+class Functions extends wpdb
+{
+    use Variables;
+
+    /**
+     * Internal function to perform the mysql_query() call
+     *
+     * @since 1.0.0
+     *
+     * @access protected
+     * @see wpdb::query()
+     *
+     * @param string $query The query to run.
+     * @param bool   $dbh_or_table  Database or table name. Defaults to false.
+     */
+    protected function _do_query( $query, $dbh_or_table = false ) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+        $dbh = $this->get_db_object( $dbh_or_table );
+
+        if ( ! $this->dbh_type_check( $dbh ) ) {
+            return false;
+        }
+
+        if ( true === $this->use_mysqli ) {
+            $result = mysqli_query( $dbh, $query );
+        } else {
+            $result = mysql_query( $query, $dbh );
+        }
+
+        // Maybe log last used to heartbeats
+        if ( ! empty( $this->check_dbh_heartbeats ) ) {
+
+            // Lookup name
+            $name = $this->lookup_dbhs_name( $dbh );
+
+            // Set last used for this dbh
+            if ( ! empty( $name ) ) {
+                $this->dbhname_heartbeats[ $name ]['last_used'] = microtime( true );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Find a dbh name value for a given $dbh object.
+     *
+     * @since 5.0.0
+     *
+     * @param object $dbh The dbh object for which to find the dbh name
+     *
+     * @return string The dbh name
+     */
+    private function lookup_dbhs_name( $dbh = false ) {
+
+        // Loop through database hosts and look for this one
+        foreach ( $this->dbhs as $dbhname => $other_dbh ) {
+
+            // Match found so return the key
+            if ( $dbh === $other_dbh ) {
+                return $dbhname;
+            }
+        }
+
+        // No match
+        return false;
+    }
+
+    /**
+     * Get the db connection object
+     *
+     * @since 1.0.0
+     *
+     * @param false|string|resource $dbh_or_table the database (the current database, the database housing the specified table, or the database of the MySQL resource)
+     */
+    protected function get_db_object( $dbh_or_table = false ) {
+
+        // No database
+        $dbh = false;
+
+        // Database
+        if ( $this->dbh_type_check( $dbh_or_table ) ) {
+            $dbh = &$dbh_or_table;
+
+            // Database
+        } elseif ( ( false === $dbh_or_table ) && $this->dbh_type_check( $this->dbh ) ) {
+            $dbh = &$this->dbh;
+
+            // Table name
+        } elseif ( is_string( $dbh_or_table ) ) {
+            $dbh = $this->db_connect( "SELECT FROM {$dbh_or_table} {$this->users}" );
+        }
+
+        return $dbh;
+    }
+
+    /**
+     * Check databse object type.
+     *
+     * @param resource|mysqli $dbh Database resource.
+     *
+     * @since 1.0.0
+     *
+     * @return bool
+     */
+    protected function dbh_type_check( $dbh ) {
+
+        if (( true === $this->use_mysqli ) && ( $dbh instanceof mysqli )) {
+            return true;
+        }
+
+        if (is_resource( $dbh )) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Add the connection parameters for a database
+     *
+     * @since 1.0.0
+     *
+     * @param array $db Default to empty array.
+     */
+    public function add_database( array $db = array() ) {
+
+        // Setup some sane default values
+        $database_defaults = array(
+            'dataset'       => 'global',
+            'write'         => 1,
+            'read'          => 1,
+            'timeout'       => 0.2,
+            'port'          => 3306,
+            'lag_threshold' => null,
+        );
+
+        // Merge using defaults
+        $db      = wp_parse_args( $db, $database_defaults );
+
+        // Break these apart to make code easier to understand below
+        $dataset = $db['dataset'];
+        $read    = $db['read'];
+        $write   = $db['write'];
+
+        // We do not include the dataset in the array. It's used as a key.
+        unset( $db['dataset'] );
+
+        // Maybe add database to array of read's
+        if ( ! empty( $read ) ) {
+            $this->ludicrous_servers[ $dataset ]['read'][ $read ][] = $db;
+        }
+
+        // Maybe add database to array of write's
+        if ( ! empty( $write ) ) {
+            $this->ludicrous_servers[ $dataset ]['write'][ $write ][] = $db;
+        }
+    }
+
+    /**
+     * Specify the dataset where a table is found
+     *
+     * @since 1.0.0
+     *
+     * @param string $dataset  Database.
+     * @param string $table    Table name.
+     */
+    public function add_table( $dataset, $table ) {
+        $this->ludicrous_tables[ $table ] = $dataset;
+    }
+
+    /**
+     * Add a callback to a group of callbacks
+     *
+     * The default group is 'dataset', used to examine queries & determine dataset
+     *
+     * @since 1.0.0
+     *
+     * @param  $callback Callback on a dataset.
+     * @param    $group   string Key name of dataset.
+     */
+    public function add_callback( $callback, $group = 'dataset' ) {
+        $this->ludicrous_callbacks[ $group ][] = $callback;
+    }
+
+    /**
+     * Determine the likelihood that this query could alter anything
+     *
+     * Statements are considered read-only when:
+     * 1. not including UPDATE nor other "may-be-write" strings
+     * 2. begin with SELECT etc.
+     *
+     * @since 1.0.0
+     *
+     * @param string $q Query.
+     *
+     * @return bool
+     */
+    public function is_write_query( $q = '' ) {
+
+        // Trim potential whitespace or subquery chars
+        $q = ltrim( $q, "\r\n\t (" );
+
+        // Possible writes
+        if ( preg_match( '/(?:^|\s)(?:ALTER|CREATE|ANALYZE|CHECK|OPTIMIZE|REPAIR|CALL|DELETE|DROP|INSERT|LOAD|REPLACE|UPDATE|SET|RENAME\s+TABLE)(?:\s|$)/i', $q ) ) {
+            return true;
+        }
+
+        // Not possible non-writes (phew!)
+        return ! preg_match( '/^(?:SELECT|SHOW|DESCRIBE|DESC|EXPLAIN)(?:\s|$)/i', $q );
+    }
+
+    /**
+     * Set a flag to prevent reading from slaves which might be lagging after a write
+     *
+     * @since 1.0.0
+     */
+    public function send_reads_to_masters() {
+        $this->srtm = true;
+    }
+
+    /**
+     * Callbacks are executed in the order in which they are registered until one
+     * of them returns something other than null
+     *
+     * @since 1.0.0
+     * @param string $group Group, key name in array.
+     * @param array  $args   Args passed to callback. Default to null.
+     */
+    public function run_callbacks( $group, $args = null ) {
+        if ( ! isset( $this->ludicrous_callbacks[ $group ] ) || ! is_array( $this->ludicrous_callbacks[ $group ] ) ) {
+            return null;
+        }
+
+        if ( ! isset( $args ) ) {
+            $args = array( &$this );
+        } elseif ( is_array( $args ) ) {
+            $args[] = &$this;
+        } else {
+            $args = array( $args, &$this );
+        }
+
+        foreach ( $this->ludicrous_callbacks[ $group ] as $func ) {
+            $result = call_user_func_array( $func, $args );
+            if ( isset( $result ) ) {
+                return $result;
+            }
+        }
+    }
+
+
+
+    /**
+     * Connect selected database
+     *
+     * @since 1.0.0
+     *
+     * @param string $dbhname Database name.
+     * @param string $host Internet address: host:port of server on internet.
+     * @param string $user Database user.
+     * @param string $password Database password.
+     *
+     * @return bool|mysqli|resource
+     */
+    protected function single_db_connect( $dbhname, $host, $user, $password ) {
+        $this->is_mysql = true;
+
+        /*
+         * Deprecated in 3.9+ when using MySQLi. No equivalent
+         * $new_link parameter exists for mysqli_* functions.
+         */
+        $new_link     = defined( 'MYSQL_NEW_LINK' ) ? MYSQL_NEW_LINK : true;
+        $client_flags = defined( 'MYSQL_CLIENT_FLAGS' ) ? MYSQL_CLIENT_FLAGS : 0;
+
+        if ( true === $this->use_mysqli ) {
+            $this->dbhs[ $dbhname ] = mysqli_init();
+
+            // mysqli_real_connect doesn't support the host param including a port or socket
+            // like mysql_connect does. This duplicates how mysql_connect detects a port and/or socket file.
+            $port           = null;
+            $socket         = null;
+            $port_or_socket = strstr( $host, ':' );
+
+            if ( ! empty( $port_or_socket ) ) {
+                $host           = substr( $host, 0, strpos( $host, ':' ) );
+                $port_or_socket = substr( $port_or_socket, 1 );
+
+                if ( 0 !== strpos( $port_or_socket, '/' ) ) {
+                    $port         = intval( $port_or_socket );
+                    $maybe_socket = strstr( $port_or_socket, ':' );
+
+                    if ( ! empty( $maybe_socket ) ) {
+                        $socket = substr( $maybe_socket, 1 );
+                    }
+                } else {
+                    $socket = $port_or_socket;
+                }
+            }
+
+            // Detail found here - https://core.trac.wordpress.org/ticket/31018
+            $pre_host = '';
+
+            // If DB_HOST begins with a 'p:', allow it to be passed to mysqli_real_connect().
+            // mysqli supports persistent connections starting with PHP 5.3.0.
+            if ( ( true === $this->persistent ) && version_compare( phpversion(), '5.3.0', '>=' ) ) {
+                $pre_host = 'p:';
+            }
+
+            mysqli_real_connect( $this->dbhs[ $dbhname ], $pre_host . $host, $user, $password, null, $port, $socket, $client_flags );
+
+            if ( $this->dbhs[ $dbhname ]->connect_errno ) {
+                $this->dbhs[ $dbhname ] = false;
+
+                return false;
+            }
+        } else {
+
+            // Check if functions exists (they do not in PHP 7)
+            if ( ( true === $this->persistent ) && function_exists( 'mysql_pconnect' ) ) {
+                $this->dbhs[ $dbhname ] = mysql_pconnect( $host, $user, $password, $new_link, $client_flags );
+            } elseif ( function_exists( 'mysql_connect' ) ) {
+                $this->dbhs[ $dbhname ] = mysql_connect( $host, $user, $password, $new_link, $client_flags );
+            }
+        }
+    }
+
+
+    /**
+     * Disconnect and remove connection from open connections list
+     *
+     * @since 1.0.0
+     *
+     * @param string $dbhname Dataname key name.
+     */
+    public function disconnect( $dbhname ) {
+
+        $k = array_search( $dbhname, $this->open_connections, true );
+        if ( ! empty( $k ) ) {
+            unset( $this->open_connections[ $k ] );
+        }
+
+        if ( $this->dbh_type_check( $this->dbhs[ $dbhname ] ) ) {
+            $this->close( $this->dbhs[ $dbhname ] );
+        }
+
+        unset( $this->dbhs[ $dbhname ] );
+    }
+
+}
--- src/ScalingWPDB.php
+++ src/ScalingWPDB.php
@@ -2,7 +2,378 @@
 
 namespace ScalingWPDB;
 
-class ScalingWPDB
-{
+use mysqli;
+use mysqli_result;
+use WP_Error;
+use wpdb;
+
+class ScalingWPDB extends Overrides {
+
+
+
+    /**
+     * Check the responsiveness of a TCP/IP daemon
+     *
+     * @since 1.0.0
+     *
+     * @param string $host Host.
+     * @param int    $port Port or socket.
+     * @param float  $float_timeout Timeout as float number.
+     * @return bool true when $host:$post responds within $float_timeout seconds, else false
+     */
+    public function check_tcp_responsiveness( $host, $port, $float_timeout ) {
+
+        // Get the cache key
+        $cache_key = $this->tcp_get_cache_key( $host, $port );
+
+        // Persistent cached value exists
+        $cached_value = $this->tcp_cache_get( $cache_key );
+
+        // Confirmed up or down response
+        if ( 'up' === $cached_value ) {
+            $this->tcp_responsive = true;
+
+            return true;
+        } elseif ( 'down' === $cached_value ) {
+            $this->tcp_responsive = false;
+
+            return false;
+        }
+
+        // Defaults
+        $errno  = 0;
+        $errstr = '';
+
+        // Try to get a new socket
+        $socket = ( WP_DEBUG )
+            ? fsockopen( $host, $port, $errno, $errstr, $float_timeout )
+            : @fsockopen( $host, $port, $errno, $errstr, $float_timeout ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+
+        // No socket
+        if ( false === $socket ) {
+            $this->tcp_cache_set( $cache_key, 'down' );
+
+            return "[ > {$float_timeout} ] ({$errno}) '{$errstr}'";
+        }
+
+        // Close the socket
+        fclose( $socket );
+
+        // Using API
+        $this->tcp_cache_set( $cache_key, 'up' );
+
+        return true;
+    }
+
+    /**
+     * Run lag cache callbacks and return current lag
+     *
+     * @since 2.1.0
+     *
+     * @return int
+     */
+    public function get_lag_cache() {
+        $this->lag = $this->run_callbacks( 'get_lag_cache' );
+
+        return $this->check_lag();
+    }
+
+    /**
+     * Should we try to ping the MySQL host?
+     *
+     * @since 4.1.0
+     *
+     * @param string $dbhname Database name.
+     *
+     * @return bool
+     */
+    public function should_mysql_ping( $dbhname = '' ) {
+
+        // Bail early if no MySQL ping
+        if ( empty( $this->check_dbh_heartbeats ) ) {
+            return false;
+        }
+
+        // Shouldn't happen
+        if ( empty( $dbhname ) || empty( $this->dbhname_heartbeats[ $dbhname ] ) ) {
+            return true;
+        }
+
+        // MySQL server has gone away
+        if ( ! empty( $this->dbhname_heartbeats[ $dbhname ]['last_errno'] ) && ( DB_SERVER_GONE_ERROR === $this->dbhname_heartbeats[ $dbhname ]['last_errno'] ) ) {
+            unset( $this->dbhname_heartbeats[ $dbhname ]['last_errno'] );
+
+            return true;
+        }
+
+        // More than 0.1 seconds of inactivity on that dbhname
+        if ( microtime( true ) - $this->dbhname_heartbeats[ $dbhname ]['last_used'] > $this->recheck_timeout ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Run lag callbacks and return current lag
+     *
+     * @since 2.1.0
+     *
+     * @return int
+     */
+    public function get_lag() {
+        $this->lag = $this->run_callbacks( 'get_lag' );
+
+        return $this->check_lag();
+    }
+
+    /**
+     * Check lag
+     *
+     * @since 2.1.0
+     *
+     * @return int
+     */
+    public function check_lag() {
+        if ( false === $this->lag ) {
+            return DB_LAG_UNKNOWN;
+        }
+
+        if ( $this->lag > $this->lag_threshold ) {
+            return DB_LAG_BEHIND;
+        }
+
+        return DB_LAG_OK;
+    }
+
+
+    /**
+     * Given a string, a character set and a table, ask the DB to check the string encoding.
+     * Classes that extend wpdb can override this function without needing to copy/paste
+     * all of wpdb::strip_invalid_text().
+     *
+     * NOTE: This must be called after LudicrousDB::db_connect, so that wpdb::dbh is set correctly
+     *
+     * @since 1.0.0
+     *
+     * @param string $string String to convert
+     * @param string $charset Character set to test against (uses MySQL character set names)
+     *
+     * @return mixed The converted string, or a WP_Error if the conversion fails
+     */
+    protected function strip_invalid_text_using_db( $string, $charset ) {
+        $query  = $this->prepare( "SELECT CONVERT( %s USING {$charset} )", $string );
+        $result = $this->_do_query( $query, $this->dbh );
+
+        // Bail with error if no result
+        if ( empty( $result ) ) {
+            return new WP_Error( 'wpdb_convert_text_failure' );
+        }
+
+        // Fetch row
+        $row = ( true === $this->use_mysqli )
+            ? mysqli_fetch_row( $result )
+            : mysql_fetch_row( $result );
+
+        // Bail with error if no rows
+        if ( ! is_array( $row ) || count( $row ) < 1 ) {
+            return new WP_Error( 'wpdb_convert_text_failure' );
+        }
+
+        return $row[0];
+    }
+
+    /** TCP Cache *************************************************************/
+
+    /**
+     * Get the cache key used for TCP responses
+     *
+     * @since 3.0.0
+     *
+     * @param string $host Host
+     * @param string $port Port or socket.
+     *
+     * @return string
+     */
+    protected function tcp_get_cache_key( $host, $port ) {
+        return "{$host}:{$port}";
+    }
+
+    /**
+     * Get the number of seconds TCP response is good for
+     *
+     * @since 3.0.0
+     *
+     * @return int
+     */
+    protected function tcp_get_cache_expiration() {
+        return 10;
+    }
+
+    /**
+     * Check if TCP is using a persistent cache or not.
+     *
+     * @since 5.1.0
+     *
+     * @return bool True if yes. False if no.
+     */
+    protected function tcp_is_cache_persistent() {
+
+        // Check if using external object cache
+        if ( wp_using_ext_object_cache() ) {
+
+            // Make sure the global group is added
+            $this->add_global_group();
+
+            // Yes
+            return true;
+        }
+
+        // No
+        return false;
+    }
+
+    /**
+     * Get cached up/down value of previous TCP response.
+     *
+     * Falls back to local cache if persistent cache is not available.
+     *
+     * @since 3.0.0
+     *
+     * @param string $key Results of tcp_get_cache_key()
+     *
+     * @return mixed Results of wp_cache_get()
+     */
+    protected function tcp_cache_get( $key = '' ) {
+
+        // Bail for invalid key
+        if ( empty( $key ) ) {
+            return false;
+        }
+
+        // Get from persistent cache
+        if ( $this->tcp_is_cache_persistent() ) {
+            return wp_cache_get( $key, $this->cache_group );
+
+            // Fallback to local cache
+        } elseif ( ! empty( $this->tcp_cache[ $key ] ) ) {
+
+            // Not expired
+            if ( ! empty( $this->tcp_cache[ $key ]['expiration'] ) && ( time() < $this->tcp_cache[ $key ]['expiration'] ) ) {
+
+                // Return value or false if empty
+                return ! empty( $this->tcp_cache[ $key ]['value'] )
+                    ? $this->tcp_cache[ $key ]['value']
+                    : false;
+
+                // Expired, so delete and proceed
+            } else {
+                $this->tcp_cache_delete( $key );
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Set cached up/down value of current TCP response.
+     *
+     * Falls back to local cache if persistent cache is not available.
+     *
+     * @since 3.0.0
+     *
+     * @param string $key Results of tcp_get_cache_key()
+     * @param string $value "up" or "down" based on TCP response
+     *
+     * @return bool Results of wp_cache_set() or true
+     */
+    protected function tcp_cache_set( $key = '', $value = '' ) {
+
+        // Bail if invalid values were passed
+        if ( empty( $key ) || empty( $value ) ) {
+            return false;
+        }
+
+        // Get expiration
+        $expires = $this->tcp_get_cache_expiration();
+
+        // Add to persistent cache
+        if ( $this->tcp_is_cache_persistent() ) {
+            return wp_cache_set( $key, $value, $this->cache_group, $expires );
+
+            // Fallback to local cache
+        } else {
+            $this->tcp_cache[ $key ] = array(
+                'value'      => $value,
+                'expiration' => time() + $expires
+            );
+        }
+
+        return true;
+    }
+
+    /**
+     * Delete cached up/down value of current TCP response.
+     *
+     * Falls back to local cache if persistent cache is not available.
+     *
+     * @since 5.1.0
+     *
+     * @param string $key Results of tcp_get_cache_key()
+     *
+     * @return bool Results of wp_cache_delete() or true
+     */
+    protected function tcp_cache_delete( $key = '' ) {
+
+        // Bail if invalid key
+        if ( empty( $key ) ) {
+            return false;
+        }
+
+        // Delete from persistent cache
+        if ( $this->tcp_is_cache_persistent() ) {
+            return wp_cache_delete( $key, $this->cache_group );
+
+            // Fallback to local cache
+        } else {
+            unset( $this->tcp_cache[ $key ] );
+        }
+
+        return true;
+    }
+
+    /**
+     * Add global cache group.
+     *
+     * Only run once, as that is all that is required.
+     *
+     * @since 4.3.0
+     */
+    protected function add_global_group() {
+        static $added = null;
+
+        // Bail if added or caching not available yet
+        if ( true === $added ) {
+            return;
+        }
+
+        // Add the cache group
+        if ( function_exists( 'wp_cache_add_global_groups' ) ) {
+            wp_cache_add_global_groups( $this->cache_group );
+        }
+
+        // Set added
+        $added = true;
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
